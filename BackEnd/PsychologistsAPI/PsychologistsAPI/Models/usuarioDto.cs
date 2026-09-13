baf9c2ed8f7f149fcb099c18b80617af2e4f6d18
t1629 <walterburique@gmail.com>
2026-09-12 21:18:35 -0300

    refactor: begin the user's CRUD

--- BackEnd/PsychologistsAPI/PsychologistsAPI/Models/usuarioDto.cs
+++ BackEnd/PsychologistsAPI/PsychologistsAPI/Models/usuarioDto.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace PsychologistsAPI.Models
 {
@@ -7,17 +8,20 @@ namespace PsychologistsAPI.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [RegularExpression("^[a-zA-Z\\s]+$", ErrorMessage = "El nombre solo puede contener letras y espacios.")]
         public string Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El email es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El email no tiene un formato válido.")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "La contraseña es obligatoria.")]
+        [MinLength(8, ErrorMessage = "La contraseña debe tener al menos 8 caracteres.")]
         public string PasswordHash { get; set; }
 
-        [Required]
-        public bool Estado { get; set; }
+        public  bool Estado { get; set; }
+       
 
 
     }
--- BackEnd/PsychologistsAPI/PsychologistsAPI/Services/usuarioServicer.cs
+++ BackEnd/PsychologistsAPI/PsychologistsAPI/Services/usuarioServicer.cs
@@ -1,6 +1,76 @@
-﻿namespace PsychologistsAPI.Services
+﻿using Data.Context;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PsychologistsAPI.Entities;
+using PsychologistsAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+
+namespace PsychologistsAPI.Services
 {
-    public class usuarioController
+    
+    public class usuarioService 
     {
+        private readonly PsychologistContext _context;
+        public usuarioService(PsychologistContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<usuarioDto?> getUser(int id)
+        {
+            var user = await _context.Usuarios.FirstOrDefaultAsync(u => u.UsuarioId == id);
+
+            if (user == null)
+
+                return null;
+
+            var dto = new usuarioDto
+            {
+                Name = user.Nombre,
+                Email = user.Email,
+                Estado = user.Estado
+
+            };
+
+            return dto;
+
+        }
+
+
+        public async Task<usuarioDto?> postUser(usuarioDto dto)
+        {
+
+            var userValid = await _context.Usuarios.FirstOrDefaultAsync(u => u.Email == dto.Email);
+
+            if (userValid != null)
+            {
+                throw new InvalidOperationException("El email ya está registrado.");
+            }
+
+            var user = new Usuario
+            {
+               Nombre = dto.Name,
+               PasswordHash = dto.PasswordHash,
+               Email = dto.Email,
+
+            };
+
+            _context.Usuarios.Add(user);
+            await _context.SaveChangesAsync();
+
+
+            return new usuarioDto
+            {
+                Id = user.UsuarioId,
+                Name = user.Nombre,
+                Email = user.Email,
+                Estado = user.Estado
+            };
+
+        }
     }
 }
