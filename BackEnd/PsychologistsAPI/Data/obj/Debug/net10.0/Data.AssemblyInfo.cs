d5671779fa8a66132cf51a5d0fdeb0f2882dee76
t1629 <walterburique@gmail.com>
2026-09-17 17:14:16 -0300

    chore: untrack dotnet build and temp files

--- BackEnd/PsychologistsAPI/Data/obj/Debug/net10.0/Data.AssemblyInfo.cs
+++ BackEnd/PsychologistsAPI/Data/obj/Debug/net10.0/Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edb09f650afa113486a120713da1b336dd5fb4b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3da38c421ee758cccfc3a9282e1528d026d9f244")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
