345a76e4f4ce506b3b6c3ee95e06b28671de32a6
t1629 <walterburique@gmail.com>
2026-09-23 12:37:41 -0300

    feat: new code line to can see how long is the bits jwt

--- BackEnd/PsychologistsAPI/PsychologistsAPI/Services/LoginService.cs
+++ BackEnd/PsychologistsAPI/PsychologistsAPI/Services/LoginService.cs
@@ -54,6 +54,10 @@ namespace PsychologistsAPI.Services
                 new Claim(ClaimTypes.Role, _User.Rol)
             };
 
+            var rawKey = _configuration["Jwt:Key"];
+            var keyBytes = Encoding.UTF8.GetBytes(rawKey);
+            Console.WriteLine($"JWT Key length: {keyBytes.Length * 8} bits");
+
 
             var key = new SymmetricSecurityKey(
                Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!)
